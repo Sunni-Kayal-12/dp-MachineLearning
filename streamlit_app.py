5104f519ed5120a5a11c92cf7b280292d51650d6
Sunni-Kayal-12 <141425978+Sunni-Kayal-12@users.noreply.github.com>
2024-09-08 08:56:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,18 @@ st.write('This app builds a Machine learning model.')
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
+
+st.write('**X**')
+X = df.drop('species', axis =1)
+X
+
+st.write('**y**')
+y
+
+with st.expander('data Visualization')
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g',color='species)
+
+# Data preparations
+with st.sidebar:
+  st.header('Input featires')
+  
